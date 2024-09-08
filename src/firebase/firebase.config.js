1c8358464c087a44fb154a0fa991c71139025042
Yash511yk <93113083+Yash511yk@users.noreply.github.com>
2024-09-08 15:43:39 +0530

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCYD8Vku0JV3Zu7zTKzUJK2VaMeWe1lTS8",
+  authDomain: "bookstore-96772.firebaseapp.com",
+  projectId: "bookstore-96772",
+  storageBucket: "bookstore-96772.appspot.com",
+  messagingSenderId: "870219002636",
+  appId: "1:870219002636:web:33135800efd0359ccd8f29",
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
